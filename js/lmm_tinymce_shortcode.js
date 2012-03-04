1db3f3f87661f45495df5c9ef36ce32a1ae437ab
Robert Harm <robert@harm.co.at>
2012-03-05 00:18:31 +0100

    working on tinymce-button (working now :-) )

--- js/lmm_tinymce_shortcode.js
+++ js/lmm_tinymce_shortcode.js
@@ -3,15 +3,14 @@
         init : function(ed, url) {
 			ed.addCommand('mm_shortcode', function() {
                                 ed.windowManager.open({
-                                        title : 'Insert/Edit link',
+                                        title : 'Insert map',
 					file : '/wp-admin/admin-ajax.php?action=get_mm_list',
 					width : 450 + parseInt(ed.getLang('example.delta_width', 0)),
-					height : 450 + parseInt(ed.getLang('example.delta_height', 0)),
+					height : 420 + parseInt(ed.getLang('example.delta_height', 0)),
                                         inline: 1
 				})
 			});
-                        
-			ed.addButton('mm_shortcode', {title : 'Add maps marker shortcode', cmd : 'mm_shortcode', image: url+'/../img/icon-menu-page.png' });
+			ed.addButton('mm_shortcode', {title : 'Insert Map', cmd : 'mm_shortcode', image: url+'/../img/icon-menu-page.png' });
         },
         createControl : function(n, cm) {
             return null;
@@ -19,4 +18,3 @@
     });
     tinymce.PluginManager.add('mm_shortcode', tinymce.plugins.mm_shortcode);
 })(jQuery);
-
