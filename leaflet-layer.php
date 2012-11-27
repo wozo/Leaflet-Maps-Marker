3f27069a1ddedf337f9130bddbcd87dd55c9f402
Robert Harm <robert@harm.co.at>
2012-11-27 20:21:58 +0100

    prepare beta2

--- leaflet-layer.php
+++ leaflet-layer.php
@@ -16,7 +16,7 @@ $current_editor_css = ($current_editor == 'simplified') ? 'display:none;' : '';
 //info: needed for backlink on edit switch
 function curPageURL() {
 	$pageURL = 'http';
-	if ($_SERVER["HTTPS"] == "on") { $pageURL .= "s"; }
+	if ( ( isset($_SERVER["HTTPS"]) && ($_SERVER["HTTPS"] == "on") ) || ($_SERVER["SERVER_PORT"] == '443') ) { $pageURL .= "s"; }
 	$pageURL .= "://";
 	if ($_SERVER["SERVER_PORT"] != "80") {
 		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
