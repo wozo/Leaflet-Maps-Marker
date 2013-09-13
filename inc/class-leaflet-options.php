cf23147b699a10e74957a3497d4c15b914368ba7
Robert Harm <robert@harm.co.at>
2013-09-13 22:11:01 +0200

    prepare v3.6.4

--- inc/class-leaflet-options.php
+++ inc/class-leaflet-options.php
@@ -9656,6 +9656,21 @@ $this->_settings['clustering_helptext2'] = array(
 		$options_new = array_merge($options_current, $new_options_defaults);
 		update_option( 'leafletmapsmarker_options', $options_new );
 		}
+		//info:  set defaults for options introduced in v3.4
+		if (get_option('leafletmapsmarker_version') == '3.6.3' )
+		{
+			$new_options_defaults = array();
+			foreach ( $this->settings as $id => $setting )
+			{
+				if ( $setting['type'] != 'heading' && $setting['type'] != 'helptext' && $setting['type'] != 'helptext-twocolumn' && $setting['type'] != 'checkbox-pro' && $setting['type'] != 'select-pro' && $setting['type'] != 'radio-pro' && $setting['type'] != 'radio-reverse-pro' && $setting['type'] != 'textarea-pro' && $setting['type'] != 'text-pro' && $setting['type'] != 'text-reverse-pro' && $setting['version'] == '3.6.4')
+				{
+				$new_options_defaults[$id] = $setting['std'];
+				}
+			}
+		$options_current = get_option( 'leafletmapsmarker_options' );
+		$options_new = array_merge($options_current, $new_options_defaults);
+		update_option( 'leafletmapsmarker_options', $options_new );
+		}		
 		/* template for plugin updates
 		//info:  set defaults for options introduced in v3.7
 		if (get_option('leafletmapsmarker_version') == '3.6.4' )
