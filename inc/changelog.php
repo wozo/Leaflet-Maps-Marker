ee8aac6e68c10a3eadd26880606f864e122cfad7
Robert Harm <robert@harm.co.at>
2013-12-20 18:36:24 +0100

    update changelog.php

--- inc/changelog.php
+++ inc/changelog.php
@@ -90,6 +90,11 @@ if (get_option('leafletmapsmarker_update_info') == 'show') {
 		<a href="https://www.mapsmarker.com/bitcoin"  target="_top">MapsMarker.com now also supports bitcoin payments</a>
 		</td></tr>
 		<tr><td>
+		<a href="' . LEAFLET_WP_ADMIN_URL . 'admin.php?page=leafletmapsmarker_pro_upgrade"  target="_top" title="' . esc_attr__('Upgrade to pro version for even more features - click here to find out how you can start a free 30-day-trial easily','lmm') . '"><img src="' . LEAFLET_PLUGIN_URL .'inc/img/icon-changelog-pro.png"></a>
+		</td><td>
+		updated markercluster codebase to v0.4 (<a href="https://github.com/Leaflet/Leaflet.markercluster/blob/master/CHANGELOG.md" target="_blank">changelog</a>)
+		</td></tr>
+		<tr><td>
 		<img src="' . LEAFLET_PLUGIN_URL .'inc/img/icon-changelog-changed.png">
 		</td><td>
 		optimized admin bar integration for WordPress 3.8+
